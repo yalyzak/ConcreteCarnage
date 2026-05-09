b8405b64feccb7b2443cf181db31c8da3c78f924
yalyzak <yaly.zak@gmail.com>
2026-05-09 18:43:23 +0300

    ...

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bereshit import Core
-from MAP import server_map, main_game_object
+from MAP import server_map, main_game_object, client_map
 from protocol import TICK
 
 player = main_game_object()
 
-Core.run([player] + server_map(), tick=TICK)
+Core.run([player] + client_map(), tick=TICK)
--- testmap.py
+++ testmap.py
@@ -1,8 +1,8 @@
 from bereshit import Core, Object, Camera, Rigidbody, BoxCollider, Vector3
 from bereshit.addons.essentials import CamController, FPS_cam
-from MAP import server_map, main_game_object
+from MAP import server_map, client_map
 from protocol import TICK
 
 player = Object(position=Vector3(0,30,0), rotation=Vector3(90,0,0)).add_component([Camera(shading="material preview"), Rigidbody(isKinematic=True), BoxCollider(is_trigger=True), FPS_cam(), CamController()])
 
-Core.run([player] + server_map(), tick=TICK)
+Core.run([player] + client_map(), tick=TICK)
